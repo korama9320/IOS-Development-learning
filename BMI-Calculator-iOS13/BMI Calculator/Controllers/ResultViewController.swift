275296f6ebcb5b464d2203305366eb271c37d1bd
abdelrahman9320 <105934344+korama9320@users.noreply.github.com>
2024-07-28 10:25:56 +0300

    .

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Roboost Mobile on 28/07/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var bmiLable: UILabel!
+    @IBOutlet weak var adviceLable: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func recalculate(_ sender: UIButton) {
+    }
+}
---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Roboost Mobile on 28/07/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
