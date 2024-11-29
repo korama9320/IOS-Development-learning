f7de8d23627c00f63eeb4a4e613f8e58b09b73cb
korama9320 <105934344+korama9320@users.noreply.github.com>
2024-11-29 14:48:46 +0200

    .

---
+++ abdo card/abdo card/abdo_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  abdo_cardApp.swift
+//  abdo card
+//
+//  Created by Roboost Mobile on 07/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct abdo_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
